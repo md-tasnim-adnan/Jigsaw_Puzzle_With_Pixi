15a33b5406fc77de0fee5c6f1127a9f125099dd2
Adnan <32835122+md-tasnim-adnan@users.noreply.github.com>
2022-12-28 15:33:27 +0600

    Bug fix

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,6 @@ import map from './map';
 
 const app = new Application({
 	view: document.getElementById("pixi-canvas") as HTMLCanvasElement,
-	resolution: window.devicePixelRatio || 1,
 	autoDensity: true,
 	backgroundColor: 0x6495ed
 });
